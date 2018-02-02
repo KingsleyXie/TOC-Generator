73384a346067d3ebca85564bae6c71b25528049a
Kingsley <kingsley.xym@gmail.com>
2018-02-03 01:55:21 +0800

    :arrow_up: Initial Same Operation To Non-JQuery Version

--- demo/toc-demo-with-jquery.js
+++ demo/toc-demo-with-jquery.js
@@ -8,107 +8,82 @@ loadJQuery();
 
 
 
-function addHeading() {
-	var val = document.getElementById("selection").value;
+const config = {
+	"title": "Table Of Contents",
+	"placeholder": "[TOC]",
+	"contentWrapper": ".post-content"
+};
 
-	if (typeof(headings) == 'undefined')
-		headings = new Array();
-	if (typeof(preHeading) == 'undefined')
-		preHeading = val;
 
-	switch (preHeading.localeCompare(val)) {
-		case 0:
-			addElement(val);
-			break;
 
-		case 1:
-			if (!headings.includes(val)) {
-				alert('This kind of heading structure is not supported.');
-				return;
-			}
+//Wait some time to load JQuery file = =
+setTimeout(function() {
+	bindClickEvents();
+}, 300);
 
-			preHeading = val;
-			while (headings.includes(preHeading)) {
-				headings.pop();
-			}
-			addElement(val);
-			break;
+function bindClickEvents() {
+	$("#btn-gen").click(function () {
+		//
+	});
 
-		case -1:
-			headings.push(preHeading);
-			preHeading = val;
-			addElement(val);
-			break;
-	}
+	$("#btn-rst").click(function () {
+		if (confirm('Are you sure to reset the contents?')) {
+			$(config.contentWrapper).html(
+				'<p>' + config.placeholder + '</p>'
+			);
+
+			//Reset heading status to have a entirely clear
+			//See line xxx to line xxx
+			delete headings;
+			delete preHeading;
+		}
+	});
+
+	$(".headings>button").click(function () {
+		var hd = this.innerText;
+
+		if (typeof(headings) == 'undefined')
+			headings = new Array();
+		if (typeof(preHeading) == 'undefined')
+			preHeading = hd;
+
+		switch (preHeading.localeCompare(hd)) {
+			case 0:
+				addElement(hd);
+				break;
+
+			case 1:
+				if (!headings.includes(hd)) {
+					alert(
+						'This kind of heading ' +
+						'structure is not supported.'
+					);
+					return;
+				}
+
+				preHeading = hd;
+				while (headings.includes(preHeading)) {
+					headings.pop();
+				}
+				addElement(hd);
+				break;
+
+			case -1:
+				headings.push(preHeading);
+				preHeading = hd;
+				addElement(hd);
+				break;
+		}
+	});
 }
 
-function addElement(val) {
-	var ele = document.createElement(val);
+function addElement(hd) {
+	var ele = document.createElement(hd);
 	var txt = document.createTextNode(
-		Math.random().toString(36).substr(2, 11) + ' ' + val
+		Math.random().toString(36).substr(2, 7) + ' ' + hd
 	);
 	ele.appendChild(txt);
 
-	var post = document.getElementById("post-content");
+	var post = document.querySelector(".post-content");
 	post.appendChild(ele);
-}
-
-function generateTOC() {
-	$(".toc").remove();
-
-	var config = {
-		"title": "Table Of Contents",
-		"contentWrapper": ".post-content"
-	};
-
-	var elements =
-		$(config.contentWrapper).find(":header")
-		.filter(":not(blockquote :header)");
-
-	if (elements.length > 0) {
-		var TOC = '<div class="toc">' + config.title + '<ul>';
-
-		var currHeading = elements[0].nodeName;
-		var records = new Array();
-
-		$.each(elements, function(key, content) {
-			var text = content.innerText;
-			var link = '<a href="#' + text + '">' + text  + '</a>';
-			content.id = text;
-
-			switch (currHeading.localeCompare(content.nodeName)) {
-				case 0:
-					TOC += '</li><li>' + link;
-					break;
-
-				case 1:
-					currHeading = content.nodeName;
-
-					if (!records.includes(currHeading)) {
-						console.warn(
-							'Warning: There may be some problem with your heading structure, ' +
-							'so the generated TOC is not guaranteed to be in right order.'
-						);
-					}
-
-					while (records.includes(currHeading)) {
-						TOC += '</li></ul>';
-						records.pop();
-					}
-					TOC += '<li>' + link;
-					break;
-
-				case -1:
-					TOC += '<ul><li>' + link;
-					records.push(currHeading);
-					currHeading = content.nodeName;
-					break;
-			}
-		});
-
-		TOC += '</ul></div>';
-		$(config.contentWrapper).find(">:first-child").before(TOC);
-	} else {
-		console.warn('No heading found to generate TOC.');
-	}
 }
